9e8f20ba15fa6384d659f707a45e14cdbfd3d399
Siddharth Pranjale <sidpranjale127@protonmail.com>
2020-07-21 09:43:45 +0530

    DRG: import stock seccomp policy

--- device.mk
+++ device.mk
@@ -385,6 +385,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+	$(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
