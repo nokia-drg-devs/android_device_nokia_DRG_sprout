1b9916a33719288bbefaa00005cdb42c5a25d81f
Aayush Gupta <aayushgupta219@gmail.com>
2020-07-21 13:57:13 +0530

    DRG: create inital overlays
    from sdm660 trees initial commit

--- device.mk
+++ device.mk
@@ -268,6 +268,10 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+	$(LOCAL_PATH)/overlay
+
 # Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
